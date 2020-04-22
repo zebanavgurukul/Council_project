fcffe3eaed8d103c38815d1fb842f60b4626d345
zebanavgurukul <zeba18@navgurukul.org>
2020-04-22 12:31:44 +0200

    commit

--- model/CouncilDB.js
+++ model/CouncilDB.js
@@ -1,5 +1,13 @@
-const knex = require("../knexFile");
+const knex = require("../knexFile"); 
 
+// 1
+let post_data = (insertdata) => {
+    return knex('council_details').insert(insertdata)
+};
 
+// 2
+let getid = (ID) => {
+    return knex('council_details').select('*').where('ID',ID)
+};
 
-module.exports = {}
+module.exports = {post_data,getid}
--- routes/Council.js
+++ routes/Council.js
@@ -2,6 +2,30 @@ const express = require("express");
 const council = express();
 const CouncilDB   = require("../model/CouncilDB")
 
+// 1
+council.post('/postdata',(req,res) => {
+    let insertdata = {
+        STUDENT_NAME : req.body.STUDENT_NAME,
+        COUNCIL_POST : req.body.COUNCIL_POST,
+        COUNCIL_START_DATE : req.body.COUNCIL_START_DATE
+    }
+    CouncilDB.post_data(insertdata)
+    .then(() => {
+        res.send('insert')
+    }).catch((err) => {
+        res.send(err)
+    })
+});
 
+// 2
+council.get('/getdata/:ID', (req,res) => {
+    var ID = req.params.ID
+    CouncilDB.getid(ID)
+    .then((Response) => {
+        res.send(Response)
+    }).catch((err) => {
+        res.send(err)
+    })
+});
 
 module.exports = council
